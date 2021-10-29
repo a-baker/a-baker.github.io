ee597599178105e24f54c87c7410483a1e16fa9a
Aaron Baker <aaronbakeremail@gmail.com>
2021-10-29 21:57:51 +0100

    Update about info to match github profile

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -2,7 +2,16 @@
   <Layout>
     <div class="container">
       <h1>About me</h1>
-      <p>I make websites. Sometimes they have more information on them than this. This one might soon.</p>
+      <p>I'm a front end developer working at CV-Library.</p>
+      <p>In my free time I'm currently working on <a href="https://watchreel.com">watchreel</a>, a movie discovery website built with Vue.</p>
+      <p>You can find my public portfolio at <a href="http://abaker.co">abaker.co</a> (be warned, I haven't updated this in a while - one for the to-do list)</p>
+      
+      <p>📫 You can find me on</p>
+      <ul>
+        <li><a href="https://www.linkedin.com/in/aaron-baker-82502377/">Linkedin</a></li>
+        <li><a href="https://www.twitter.com/_AaronBaker">Twitter</a></li>
+        <li><a href="https://codepen.io/abaker">CodePen</a></li>
+      </ul>
     </div>
   </Layout>
 </template>
